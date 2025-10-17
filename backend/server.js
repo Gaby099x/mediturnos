65380f831360bd48f6167503010f855cba722430
Gaby099x <grabianjugadores@gmail.com>
2025-10-17 20:47:11 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-
 import usuariosRoutes from "./routes/usuarios.js";
 import turnosRoutes from "./routes/turnos.js";
 
@@ -11,9 +10,7 @@ app.use(express.json());
 app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/turnos", turnosRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API del Consultorio funcionando ");
-});
+app.get("/", (req, res) => res.send("API del Consultorio funcionando "));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
